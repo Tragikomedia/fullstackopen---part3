9e4f1f5b364281371c3a08d1fc31db7079dc93df
Tragikomedia <kamilk9625@gmail.com>
2021-03-22 19:13:41 +0100

    Finished 3.4.

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { persons } = require("./db");
+const fs = require('fs');
+let { persons } = JSON.parse(fs.readFileSync('./db.json'));
 
 router.get("/persons", (req, res) => {
   res.json(persons);
@@ -10,6 +11,13 @@ router.get("/persons/:id", (req, res) => {
   const person = persons.find((p) => p.id === id);
   if (!person) res.status(404).end("404 Not Found");
   res.json(person);
+});
+
+router.delete("/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(p => p.id !== id);
+    fs.writeFileSync('./db.json', JSON.stringify({persons}));
+    res.status(204);
 });
 
 module.exports = router;
--- info.js
+++ info.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-
-const { persons } = require("./db");
+const fs = require("fs");
 
 router.get("/info", (req, res) => {
+  let { persons } = JSON.parse(fs.readFileSync("./db.json"));
   const len = persons.length;
   const date = new Date().toString();
-  res.set("Content-Type", "text/html; charset=utf-8")
+  res.set("Content-Type", "text/html; charset=utf-8");
   res.end(`<p>Phonebook has info for ${len} people</p><br><p>${date}`);
 });
 
