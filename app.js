31441b870dd9d0313266f0ebedd3ceb65b4d1eca
Tragikomedia <kamilk9625@gmail.com>
2021-03-23 19:05:16 +0100

    Preparing build.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token("body", (req, res) =>
   req.method === "POST" ? JSON.stringify(req.body) : ""
