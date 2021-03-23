223a098d45c35e527b5466a466822003d2a6237b
Tragikomedia <kamilk9625@gmail.com>
2021-03-23 17:27:38 +0100

    Finised 3.8.

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const app = express();
-
-app.use(morgan('tiny'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 
-app.use('/', require('./info'));
-app.use('/api', require('./api'));
+morgan.token("body", (req, res) =>
+  req.method === "POST" ? JSON.stringify(req.body) : ""
+);
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+app.use("/", require("./info"));
+app.use("/api", require("./api"));
 
 app.use((req, res) => {
-    res.status(404).end("404 Not Found");
-})
+  res.status(404).end("404 Not Found");
+});
 module.exports = app;
